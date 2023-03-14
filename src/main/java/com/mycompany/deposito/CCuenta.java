eaa06e02a034a33be17ca87aa5dff6d6e800efbf
Angelsqual <126610763+Angelsqual@users.noreply.github.com>
2023-03-14 10:01:23 +0100

    Update CCuenta.java

--- src/main/java/com/mycompany/deposito/CCuenta.java
+++ src/main/java/com/mycompany/deposito/CCuenta.java
@@ -5,23 +5,45 @@
 package com.mycompany.deposito;
 
 /**
- *
+ * Esta clase representa una cuenta corriente bancaria.
+ * @since 28 de febrero de 2023
+ * @version Netbeans 17.0
  * @author Angelsqual
  */
 public class CCuenta {
 
-    public CCuenta(String antonio_López, String string, int i, int i0) {
+    /**
+     * Constructor de la clase CCuenta.
+     * @param titular El titular de la cuenta.
+     * @param cuenta El número de cuenta.
+     * @param saldo El saldo inicial de la cuenta.
+     * @param tipoInteres El tipo de interés de la cuenta.
+     */
+    public CCuenta(String titular, String cuenta, int saldo, int tipoInteres) {
+        // TODO: Implementar constructor
     }
 
+    /**
+     * Devuelve el estado actual de la cuenta.
+     * @return El saldo actual de la cuenta.
+     */
     double estado() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-    void retirar(int i) {
+    /**
+     * Permite retirar una cantidad de dinero de la cuenta.
+     * @param cantidad La cantidad de dinero a retirar.
+     */
+    void retirar(int cantidad) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
-    void ingresar(int i) {
+    
+    /**
+     * Permite ingresar una cantidad de dinero en la cuenta.
+     * @param cantidad La cantidad de dinero a ingresar.
+     */
+    void ingresar(int cantidad) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
